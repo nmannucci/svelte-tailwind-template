0f8b85cd04a58ad6fd17d5e4d871a4eb7b93410b
nmannucci <67347038+nmannucci@users.noreply.github.com>
2021-02-09 21:10:11 -0800

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+  import Toggle from "./components/Toggle.svelte";
+  let darkMode = true;
+</script>
+
+<style lang="postcss">
+  a {
+    @apply bg-pink-600 rounded text-white;
+  }
+  a:hover {
+    @apply bg-pink-500;
+  }
+</style>
+
+<main class="{darkMode ? 'dark' : ''}">
+  <div class="w-full bg-pink-200 dark:bg-gray-800">
+    <div class="container mx-auto text-center py-4">
+      <h1 class="font-bold text-2xl dark:text-white">Hello World!</h1>
+      <span class="dark:text-white">Welcome to Svelte-Tailwind starter.</span>
+      <div class="my-4">
+        <a
+          target="_blank"
+          class="p-2 m-2"
+          href="https://github.com/lohanidamodar/svelte-tailwind-starter">
+          Star Github
+        </a>
+      </div>
+      <Toggle label="Dark Mode" bind:value="{darkMode}" />
+    </div>
+  </div>
+</main>
